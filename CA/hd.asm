a1aee9b3e07d9a33267086099f5e3cdce7d170fe
Aringas Civilka <aringas15@gmail.com>
2025-10-23 02:04:05 +0300

    Wrote and added a hex dump program in 8086 assembly. Created a Computer Architecture folder for it and updated .gitignore to include a bit more programming languages.

---
+++ CA/hd.asm
@@ -0,0 +1,258 @@
+    ;
+    ; A hex dump program written in 8086 assembly:
+    ; used dosbox, tasm and tlink.
+    ;
+    ; Usage:
+    ; hd.asm input.file output.file
+    ;
+
+.model small
+.stack 100h
+.data
+    fn_in db 255 dup(0)
+    fn_out db 255 dup(0)
+    fd_in dw ?
+    fd_out dw ?
+    buff_in db 512 dup(?)
+    buff_out db 1728 dup(?)     ; (512 * 3) + ((512 * 3) / 16) * 2
+                                ; One byte is chars [0; f] + space and
+                                ; each line of (16) bytes in hex requires 2 bytes for \n
+    err_msg db "Invalid input$"
+    usage_msg db "Usage: dasm.exe input.exe output.asm", 0dh, 0ah
+              db "Argument filename lengths must be in range [1; 12]$"
+    file_err_msg db "Something went wrong with one of the files$"
+    newl db 0dh, 0ah, '$'
+
+; MACROS
+print_string macro string
+    mov ah, 09h
+    mov dx, offset string
+    int 21h
+endm
+
+print_char macro char
+    mov ah, 02h
+    mov dl, char
+    int 21h
+endm
+
+terminate macro
+    mov ah, 4ch
+    int 21h
+endm
+
+.code
+start:
+    mov ax, @data
+    mov ds, ax
+    
+    xor cx, cx
+    mov cl, es:[80h]
+
+    ; read first arg
+    xor bx, bx
+    xor si, si
+    mov di, offset fn_in
+    call get_arg
+    cmp bx, 0
+    je print_err
+    cmp bx, 12
+    jg print_err
+    ; check for /?
+    cmp bx, 2
+    jne continue
+    cmp [fn_in], '/'
+    jne continue
+    cmp [fn_in + 1], '?'
+    je print_usage
+
+continue:
+    ; read second arg
+    xor bx, bx
+    mov di, offset fn_out
+    call get_arg
+    cmp bx, 0
+    je print_err
+    cmp bx, 12
+    jg print_err
+
+    ; open file 'fn_in' for reading
+    mov ax, 3d00h
+    mov dx, offset fn_in
+    int 21h
+    mov fd_in, ax
+    jc file_err
+    
+    ; create or open file 'fn_out' for writing
+    mov ax, 3c00h
+    xor cx, cx
+    mov dx, offset fn_out
+    int 21h
+    mov fd_out, ax
+    jc file_err
+
+    call hex_dump
+
+    ; close both files
+    mov ax, 3e00h
+    mov bx, fd_out
+    int 21h
+    mov ax, 3e00h
+    mov bx, fd_in
+    int 21h
+
+done:
+    terminate
+
+print_err:
+    print_string err_msg
+    print_string newl
+    print_string usage_msg
+    terminate
+
+print_usage:
+    print_string usage_msg
+    terminate
+
+file_err:
+    print_string file_err_msg
+    ; **
+    ;   Doesn't work, intended to print the
+    ;   hex value of the file error code in ax
+    ; **
+    ; call byte_to_hex
+    ; mov dl, ah
+    ; print_char dl
+    ; print_char al
+    terminate
+
+hex_dump proc
+    ; Loop through input file in blocks of 512 bytes:
+    ; fd_in -> buff_in.
+    ; Loop through the block and write each byte's hex
+    ; value to buff_out and finally buff_out -> fd_out.
+
+    .read_loop:
+        ; read from file
+        mov dx, offset buff_in
+        mov ax, 3f00h
+        mov bx, fd_in
+        mov cx, 512
+        int 21h
+        jc file_err
+
+        mov cx, ax
+        jcxz .done
+
+        mov si, offset buff_in
+        mov di, offset buff_out
+        xor dl, dl ; column counter 0-16
+
+        .hex_loop:
+            mov al, [si]
+            call byte_to_hex
+            mov [di], ah
+            mov [di + 1], al
+            mov byte ptr [di + 2], ' '
+            add di, 3
+            inc si
+
+            inc dl
+            cmp dl, 16
+            jne .no_nl
+                mov byte ptr [di], 0dh
+                inc di
+                mov byte ptr [di], 0ah
+                inc di
+                xor dl, dl
+            .no_nl:
+
+        loop .hex_loop
+
+        ; write to file, compute byte count to write:
+        ; cx = offset buff_out - di
+        mov ax, di
+        sub ax, offset buff_out
+        mov cx, ax
+        mov dx, offset buff_out
+        mov ax, 4000h
+        mov bx, fd_out
+        int 21h
+        jc file_err
+
+    jmp .read_loop
+
+    .done:
+        ret
+hex_dump endp
+
+byte_to_hex proc
+    ; take byte in al, output higher 4 bit's
+    ; hex value in ah and the lower 4 bit's in al
+    mov ah, al
+    shr ah, 4
+    and al, 15
+
+    cmp ah, 9
+    jg .upper_ah
+    add ah, '0'
+    jmp .skip_ah
+    .upper_ah:
+        sub ah, 10
+        add ah, 'a'
+    .skip_ah:
+
+    cmp al, 9
+    jg .upper_al
+    add al, '0'
+    jmp .skip_al
+    .upper_al:
+        sub al, 10
+        add al, 'a'
+    .skip_al:
+    
+    ret
+byte_to_hex endp
+
+get_arg proc
+    ; Input:
+    ; si - offset from start of arguments string
+    ; cx - total length or arguments string at 80h
+    ; di - destination address for argument string
+    ; Output:
+    ; bx - argument's length
+
+    .skip_spaces:
+        cmp si, cx
+        jge .end_arg
+        mov al, es:[81h + si]
+        cmp al, ' '
+        je .inc_si
+        cmp al, 9
+        je .inc_si
+        jmp .copy
+    .inc_si:
+        inc si
+        jmp .skip_spaces
+
+    .copy:
+        cmp si, cx
+        jge .end_arg
+        mov al, es:[81h + si]
+        cmp al, ' '
+        je .end_arg
+        cmp al, 9
+        je .end_arg
+        je .end_arg
+
+        mov [di], al
+        inc bx
+        inc di
+        inc si
+        jmp .copy
+
+    .end_arg:
+        ret
+get_arg endp
+
+end start
