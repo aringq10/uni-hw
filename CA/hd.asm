800731ae6bc565269f60f81111e659028e1cfe02
Aringas Civilka <aringas15@gmail.com>
2025-10-23 15:06:29 +0300

    Rewrote byte_to_hex function in CA/hd.asm to use a hex_string for forming the byte hex value instead of computing it manually.

--- CA/hd.asm
+++ CA/hd.asm
@@ -17,10 +17,11 @@
     buff_out db 1728 dup(?)     ; (512 * 3) + ((512 * 3) / 16) * 2
                                 ; One byte is chars [0; f] + space and
                                 ; each line of (16) bytes in hex requires 2 bytes for \n
+    hex_string db "0123456789abcdef$"
     err_msg db "Invalid input$"
     usage_msg db "Usage: dasm.exe input.exe output.asm", 0dh, 0ah
               db "Argument filename lengths must be in range [1; 12]$"
-    file_err_msg db "Something went wrong with one of the files$"
+    file_err_msg db "Something went wrong with one of the files. Error code: $"
     newl db 0dh, 0ah, '$'
 
 ; MACROS
@@ -112,18 +113,16 @@ print_err:
 
 print_usage:
     print_string usage_msg
+
     terminate
 
 file_err:
     print_string file_err_msg
-    ; **
-    ;   Doesn't work, intended to print the
-    ;   hex value of the file error code in ax
-    ; **
-    ; call byte_to_hex
-    ; mov dl, ah
-    ; print_char dl
-    ; print_char al
+    mov bl, al
+    call byte_to_hex
+    print_char bh
+    print_char bl
+    print_char 'h'
     terminate
 
 hex_dump proc
@@ -149,10 +148,10 @@ hex_dump proc
         xor dl, dl ; column counter 0-16
 
         .hex_loop:
-            mov al, [si]
+            mov bl, [si]
             call byte_to_hex
-            mov [di], ah
-            mov [di + 1], al
+            mov [di], bh
+            mov [di + 1], bl
             mov byte ptr [di + 2], ' '
             add di, 3
             inc si
@@ -187,30 +186,23 @@ hex_dump proc
 hex_dump endp
 
 byte_to_hex proc
-    ; take byte in al, output higher 4 bit's
-    ; hex value in ah and the lower 4 bit's in al
-    mov ah, al
-    shr ah, 4
-    and al, 15
+    ; take byte in bl, output higher 4 bit's
+    ; hex value in bh and the lower 4 bit's in bl
+    ; *uses hex_string
+    push dx
+    mov bh, bl
+    shr bh, 4
+    and bl, 15
+    push bx
+    xor bh, bh
 
-    cmp ah, 9
-    jg .upper_ah
-    add ah, '0'
-    jmp .skip_ah
-    .upper_ah:
-        sub ah, 10
-        add ah, 'a'
-    .skip_ah:
-
-    cmp al, 9
-    jg .upper_al
-    add al, '0'
-    jmp .skip_al
-    .upper_al:
-        sub al, 10
-        add al, 'a'
-    .skip_al:
-    
+    mov dl, [hex_string + bx]
+    pop bx
+    mov bl, bh
+    xor bh, bh
+    mov bh, [hex_string + bx]
+    mov bl, dl
+    pop dx
     ret
 byte_to_hex endp
 
